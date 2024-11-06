334286d574a130be15a19b86d64b9a7bae73ea5e
Ruizhe Liu <kaofex98@gmail.com>
2024-11-06 17:13:38 +0800

    Update constants.ts

--- backend/src/manage-item/constants.ts
+++ backend/src/manage-item/constants.ts
@@ -1,18 +1,23 @@
 export const REGION = "us-east-1";
 export const TABLE_NAMES = {
-  ITEMS: "dev-items",
+  ITEMS: "dev-items3",
+  USERS: "dev-users3",
 };
+
 export const ITEM_STATES = {
   ACTIVE: "active",
   INACTIVE: "inactive",
   UNPUBLISHED: "unpublished",
   COMPLETED: "completed",
-} as const;
+};
+
 export const MESSAGES = {
   ITEM_NOT_FOUND: "Item not found",
+  SELLER_NOT_FOUND: "Seller not found",
   INVALID_STATE: "Invalid item state for this action",
   PUBLISH_SUCCESS: "Item published successfully",
   UNPUBLISH_SUCCESS: "Item unpublished successfully",
   REVIEW_SUCCESS: "Item submitted for review successfully",
   INTERNAL_SERVER_ERROR: "Internal Server Error",
+  UNAUTHORIZED: "You are not authorized to perform this action",
 };
