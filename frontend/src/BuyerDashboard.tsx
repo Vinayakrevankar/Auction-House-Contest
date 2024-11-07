7344f4c83388e9a8616adf29c5bad97187f31258
vinayakrevankar <vinayakanrevankar@gmail.com>
2024-11-07 16:18:50 -0500

    frontend/ - updated message

--- frontend/src/BuyerDashboard.tsx
+++ frontend/src/BuyerDashboard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const BuyerDashboard = () => {
-  return <h1>Welcome to the Buyer Dashboard</h1>;
+  return <h1>Welcome to the Buyer Dashboard - Under development</h1>;
 };
 
 export default BuyerDashboard;
