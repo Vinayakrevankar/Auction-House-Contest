6ac81ecce1b5c3d10c1a142e2d4e478c2e7e8cca
vinayakrevankar <vinayakanrevankar@gmail.com>
2024-11-07 16:20:19 -0500

    frontend/ - updated message

--- frontend/src/BuyerDashboard.tsx
+++ frontend/src/BuyerDashboard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LogoutButton from './components/LogoutButton';
 
 const BuyerDashboard = () => {
-  return <h1>Welcome to the Buyer Dashboard - Under development  <LogoutButton/></h1>;
+  return <h1>Welcome to the Buyer Dashboard - Under Construction  <LogoutButton/></h1>;
 };
 
 export default BuyerDashboard;
