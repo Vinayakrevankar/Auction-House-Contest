b37ffe850e03b9dcdb52f3321b20206b96558d40
vinayakrevankar <vinayakanrevankar@gmail.com>
2024-11-07 16:19:47 -0500

    added logout button on buyer screen

--- frontend/src/BuyerDashboard.tsx
+++ frontend/src/BuyerDashboard.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import LogoutButton from './components/LogoutButton';
 
 const BuyerDashboard = () => {
-  return <h1>Welcome to the Buyer Dashboard - Under development</h1>;
+  return <h1>Welcome to the Buyer Dashboard - Under development  <LogoutButton/></h1>;
 };
 
 export default BuyerDashboard;
