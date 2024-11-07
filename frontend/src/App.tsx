631d0a96e11a6bb5a30c7771463e64aa73e56bd4
vinayakrevankar <vinayakanrevankar@gmail.com>
2024-11-06 23:16:21 -0500

    updated manage-user handler and spec

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
 import { useState } from 'react';
 import { AuthContext } from './AuthContext';
 import LandingPage from './LandingPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SellerDashboard from './SellerDashboard';
 
 const App = () => {
   const [userJWTToken, setUserJWTToken] = useState<string | null>(null);
 
   return (
     <AuthContext.Provider value={{ userJWTToken, setUserJWTToken }}>
-      <div className="App">
-        <LandingPage />
-      </div>
+      <Router>
+            <Routes>
+                <Route path="/Auction-House-Contest/" element={<LandingPage />} />
+                <Route path="/Auction-House-Contest/seller-dashboard" element={<SellerDashboard />} />
+            </Routes>
+        </Router>
     </AuthContext.Provider>
   );
 };
