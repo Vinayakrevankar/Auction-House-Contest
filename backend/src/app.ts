5e5425e25b7d8c4f74f3586e2ff2824e14309caa
Rickyash <1145967792@qq.com>
2024-12-09 10:13:50 -0500

    Update the app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -166,7 +166,7 @@ app.get("/api/buyers/:buyerId/bids", authFilterMiddleware, (req, res) => {
 });
 
 //working
-app.get("/api/items/recently-sold", (req, res) =>{
+app.get("/api/items/recently-sold",authFilterMiddleware, (req, res) =>{
   getRecentlySoldItems(req, res);
 });
 
